47f42980a691b9268f2302473e4be61e714680f5
Samir Harry Huamán Patricio <samirharry13@gmail.com>
2019-05-02 00:24:16 -0500

    hl

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -10,8 +10,5 @@ module.exports.welcomeSystem = (req, res) => {
 module.exports.register = (req,res) =>{
 	let body = req.body;
 	console.log(body);
-	return res.status(200).json({
-		ok: true,
-		message: "HUARISEXO"
-	})
+	return res.status(200).send("HUARISEXO");
 }
